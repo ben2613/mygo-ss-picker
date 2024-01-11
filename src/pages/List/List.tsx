1a3c296dc6edd18b48aed534ea9e9037b081b2ad
ben2613 <sosgyuki@gmail.com>
2024-01-12 00:56:15 +0800

    semi ok

--- src/pages/List/List.tsx
+++ src/pages/List/List.tsx
@@ -1,13 +1,23 @@
 import { ImageData } from "@/assets/imageData";
 import ImageCell from "@/components/ImageCell";
+import { Input, Navbar, Typography } from "@material-tailwind/react";
+import { useState } from "react";
 
 
-const List = () => (
-    <div className="flex flex-wrap">
-        {ImageData.map((image, idx) => (
-            !image.isDeleted && <ImageCell src={image.path} alt={image.line} key={idx} />
-        ))}
+const List = () => {
+    const [search, setSearch] = useState<string>("")
+    return <div className="min-h-screen">
+        <Navbar className="sticky top-0 z-10 w-full max-w-full px-4 py-2">
+            <div className="flex items-center justify-between">
+                <Input label="Search" placeholder="為甚麼要演奏春日影" onChange={e => {setSearch(e.target.value)}} />
+            </div>
+        </Navbar>
+        <div className="flex flex-wrap">
+            {ImageData.filter(datum => datum.line.includes(search)).map((image, idx) => (
+                !image.isDeleted && <ImageCell src={image.path} alt={image.line} key={idx} />
+            ))}
+        </div>
     </div>
-);
+};
 
 export default List;

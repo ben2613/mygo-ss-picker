070c308d71146e84f90aea1d77b06ee1ed3f65e9
ben2613 <sosgyuki@gmail.com>
2024-01-12 18:01:10 +0800

    feat: long press thumbnail to copy

--- src/components/Thumbnail.tsx
+++ src/components/Thumbnail.tsx
@@ -1,7 +1,9 @@
 // Thumbnail element accepting react props in typescript
 
+import { copyImageToClipboard } from "copy-image-clipboard";
 import React from "react";
 import { Link } from "react-router-dom";
+import { useLongPress } from "react-use";
 
 interface Props {
     src: string;
@@ -9,10 +11,20 @@ interface Props {
 }
 
 const Thumbnail: React.FC<Props> = ({ src, alt }) => {
+    const fullURL = new URL(
+        src,
+        window.location.protocol +
+            "/" +
+            window.location.host
+    ).toString()
+    const onLongPress = () => {
+        copyImageToClipboard(fullURL);
+    }
+    const longPressEvent = useLongPress(onLongPress, {isPreventDefault: true, delay: 300});
     return (
         <div className="overflow-hidden rounded-lg">
-            <Link to={"/fullscreen" + src} >
-                <img
+            <Link {...longPressEvent} to={"/fullscreen" + src} >
+                <img 
                     src={src}
                     alt={alt}
                     className="object-cover w-full transition-transform hover:scale-110"

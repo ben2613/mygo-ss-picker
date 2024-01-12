4526c94d0e901969cb692b93fdc981b53a155d4f
ben2613 <sosgyuki@gmail.com>
2024-01-12 17:39:32 +0800

    function test

--- src/pages/Fullscreen/Fullscreen.tsx
+++ src/pages/Fullscreen/Fullscreen.tsx
@@ -1,10 +1,14 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { ImageData } from "@/assets/imageData";
-import { Button } from "@material-tailwind/react";
+import Button from "@material-tailwind/react/components/Button";
+import { copyImageToClipboard } from "copy-image-clipboard";
+import JsFileDownloader from "js-file-downloader";
 
 function copyURL(url: string) {
     navigator.clipboard.writeText(url);
 }
+
+
 
 const Fullscreen = () => {
     const params = useParams<{ "*": string }>();
@@ -15,26 +19,28 @@ const Fullscreen = () => {
         line: "404",
     };
 
+    const fullURL = new URL(
+        image.path,
+        window.location.protocol +
+            "/" +
+            window.location.host
+    ).toString()
+
     return (
         <div className="flex flex-wrap w-screen gap-2">
             <img src={image?.path} alt={image.line} className="w-full" />
             <Button
-                onClick={() => {
-                    copyURL(
-                        new URL(
-                            image.path,
-                            window.location.protocol +
-                                "/" +
-                                window.location.host
-                        ).toString()
-                    );
-                }}
+                onClick={() => copyURL(fullURL)}
                 className="flex-auto w-full md:w-auto"
-            >
+                >
                 Copy URL
             </Button>
-            <Button className="flex-auto w-full md:w-auto">Copy Image</Button>
-            <Button className="flex-auto w-full md:w-auto">Download</Button>
+            <Button className="flex-auto w-full md:w-auto"
+                onClick={() => copyImageToClipboard(fullURL)}
+            >
+                Copy Image
+            </Button>
+            <Button onClick={() => new JsFileDownloader({url: fullURL})} className="flex-auto w-full md:w-auto">Download</Button>
             <Button onClick={() => navigate(-1)} variant="outlined" color="blue"
                 className="flex-auto w-full md:w-auto"
             >
